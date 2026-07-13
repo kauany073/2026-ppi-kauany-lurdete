b918ecf31c27bb54f9a0f26d9ff689a5236bade7
kau <b.kauany@escolar.ifrn.edu.br>
2026-07-13 10:17:32 -0300

    feat: implement Configuration section from Flask tutorial

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,24 @@
+import os
+
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else: 
+        app.config.from_mapping(test_config)
+
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     @app.route("/")
     def hello():
